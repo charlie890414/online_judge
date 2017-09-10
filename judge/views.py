51004cb73ed96895975dad84bb2cc79bbe6ae266
charlie890414 <charlie890414@gmail.com>
2017-09-10 15:09:17 +0800

    account

--- judge/views.py
+++ judge/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+...
+from django.contrib import auth  # 別忘了import auth
+
+...
+def login(request):
+
+    if request.user.is_authenticated():
+        return HttpResponseRedirect('/index/')
+
+    name = requst.POST.get('name','')
+    email = request.POST.get('email', '')
+    password = request.POST.get('password', '')
+
+    user = auth.authenticate(name=name,email=email, password=password)
+
+    if user is not None and user.is_active:
+        auth.login(request, user)
+        return HttpResponseRedirect('/index/')
+    else:
+        return render_to_response('singin.html')
+
+def index(request):
+    return render_to_response('index.html',locals())
