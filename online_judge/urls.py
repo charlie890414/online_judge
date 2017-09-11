20783ec71f46cf6ee2a4d3fd3d2e398f00578af5
Gary GMo <boss.su2000@gmail.com>
2017-09-12 00:19:06 +0800

    sign in sign ot

--- online_judge/urls.py
+++ online_judge/urls.py
@@ -17,10 +17,12 @@ from django.conf.urls import url
 from django.contrib import admin
 from judge.views import index
 from judge.views import signup
-from judge.views import signin
+from judge.views import signin, signout
+
 urlpatterns = [
-    url(r'^admin', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
     url(r'^$', index),
     url(r'^signup', signup),
     url(r'^signin', signin),
+    url(r'^signout', signout),
 ]
